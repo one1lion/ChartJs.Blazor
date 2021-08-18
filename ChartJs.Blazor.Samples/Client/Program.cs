fb3932c9cde107c2d158e6eccc50e5f7284dd71e
one1lion <i_am_listening@hotmail.com>
2021-08-18 09:56:38 -0600

    Tried to refactor the Client project to get the switch to work; currently only ?mode=server displays the page correctly

--- ChartJs.Blazor.Samples/Client/Program.cs
+++ ChartJs.Blazor.Samples/Client/Program.cs
@@ -1,13 +1,9 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using ChartJs.Blazor.Samples.Shared;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ChartJs.Blazor.Samples.Client
 {
